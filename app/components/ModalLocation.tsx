4308e2d8edbcc2da28594d9ce15e359ceec9f72b
oxnut134 <oka70301@gmail.com>
2026-04-07 09:27:24 +0900

    update position center

--- app/components/ModalLocation.tsx
+++ app/components/ModalLocation.tsx
@@ -223,11 +223,13 @@ export default function ModalLocation({ modal, setCurrentMarker, setOpenedModalL
                 style={{
                     //position: 'absolute',
                     position: 'fixed',
-                    top: `${localPos.y - 15}px`, // 少し余裕を持たせる
-                    left: `${localPos.x + 15}px`,
-                    transform: 'translate(0, -100%)',
+                    //top: `${localPos.y - 15}px`, // 少し余裕を持たせる
+                    //left: `${localPos.x + 15}px`,
+                    //transform: 'translate(0, -100%)',
                     //transform: 'translate(-50%, -50%)',
-                    zIndex: 9999,
+                    top: '50%',          // 💡 変数を使わず「50%」と直接書く
+                    left: '50%',         // 💡 変数を使わず「50%」と直接書く
+                    transform: 'translate(-50%, -50%)', // 💡 真ん中寄せzIndex: 9999,
                     backgroundColor: 'white',
                     padding: '16px', // 12pxから16pxへ。余白に呼吸を持たせる
                     borderRadius: '10px',
