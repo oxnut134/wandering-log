c30d1f38e045dbf08face02f36539e6aa9c2eaf3
oxnut134 <oka70301@gmail.com>
2026-04-30 15:29:49 +0900

    Add group forcus function

--- app/components/ModalLocation.tsx
+++ app/components/ModalLocation.tsx
@@ -348,6 +348,7 @@ export default function ModalLocation({ modal, isFocused, onFocus, setCurrentMar
                     top: `${localPos.y - 15}px`, // 少し余裕を持たせる
                     left: `${localPos.x + 15}px`,
                     transform: 'translate(0, -100%)',
+                    touchAction: 'none',
                     zIndex: isFocused ? 2000 : 1000,
                     border: isFocused ? '2px solid #ff4444' : '1px solid #ccc',
                     boxShadow: isFocused ? '0 10px 30px rgba(0,0,0,0.2)' : 'none',
