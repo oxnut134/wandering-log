c851990f4ed78e0dba27c3afb8f3f578e9a0ecd2
oxnut134 <oka70301@gmail.com>
2026-05-01 08:54:10 +0900

    Add mobile group forcus function

--- app/components/ModalLocation.tsx
+++ app/components/ModalLocation.tsx
@@ -401,12 +401,12 @@ export default function ModalLocation({ modal, isFocused, onFocus, setCurrentMar
                     </button>
                 </div>
                 <p style={{ fontSize: '11px', color: '#777', marginBottom: '5px' }}>
-                    {/*緯度: {Number(openedModalGoogle.latitude || 0).toFixed(4)} /
-                    経度: {Number(openedModalGoogle.longitude || 0).toFixed(4)}*/}
+                    緯度: {Number(openedModalGoogle.latitude || 0).toFixed(4)} /
+                    経度: {Number(openedModalGoogle.longitude || 0).toFixed(4)}
                     {/*緯度: {localPos.x} /
                     経度: {localPos.y}*/}
-                    緯度: {localPos.xCheck} /
-                    経度: {localPos.yCheck}
+                    {/*緯度: {localPos.xCheck} /
+                    経度: {localPos.yCheck}*/}
                 </p>
                 <input
                     style={{
