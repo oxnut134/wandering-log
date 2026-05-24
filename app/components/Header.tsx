23678a43e71f24597b252ffdc40c5a9bcc8c79d6
oxnut134 <oka70301@gmail.com>
2026-05-24 10:28:58 +0900

    Add mobile header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -27,7 +27,7 @@ export default function Header({ isDesktop, setIsDesktop }: any) {
         <>
             {/* 🎯 修正1: flex flex-col justify-center を追加し、45pxの「ど真ん中」にコンテンツを配置 */}
             {/*<div className="w-1/4 h-[45px] flex flex-col justify-center bg-white border-b border-gray-200 py-0 px-3 shadow-sm mb-0">*/}
-        {isDesktop ? (<div
+            {isDesktop ? (<div
 
                 className={`${isMapPage ? "w-full h-auto mt-0 pt-1 rounded-sm " : "w-full h-auto mt-0 py-1"
                     } flex flex-col items-center justify-center bg-white border border-3 border-[#388778] px-3 shadow-sm mb-0 transition-all duration-200 ml-5`}
@@ -69,23 +69,27 @@ export default function Header({ isDesktop, setIsDesktop }: any) {
                     </div>
                 </div>
             </div>
-            ):(
-            <div className={`${isMapPage ? "w-full h-auto mt-0 pt-1 rounded-sm " : "w-full h-auto mt-0 py-1"} flex flex-col items-center justify-center bg-white border border-3 border-[#388778] px-1.5 shadow-sm mb-0 transition-all duration-200 ml-0`}>
-                {userName && (
-                    <div className="text-[9px] text-zinc-400 leading-none mb-0.5 pl-0.5 w-full text-left">
-                        Logged in as: <span className="text-[12px] text-orange-600 font-bold">{userName}</span>
-                    </div>
-                )}
-                <div className="w-full flex items-center justify-between gap-1 mb-1 py-0">
-                    <img src="/sdesign_00053.png" alt="買い物かご" className="w-6 aspect-square rounded-full object-contain" />
-                    <span className="flex-1 text-[18px] italic text-[#388778] font-black tracking-wider text-center whitespace-nowrap">Wandering Log</span>
-                    <div className="text-[12px] font-bold text-gray-500 text-right whitespace-nowrap">
-                        {currentPage === "login" && <button onClick={() => window.location.href = "/register"}>新規登録</button>}
-                        {currentPage === "register" && <button onClick={() => window.location.href = "/login"}>ログイン</button>}
-                        {pathname === "/" && <button onClick={handleLogout} className="hover:text-red-500 font-black transition-colors">ログアウト</button>}
+            ) : (
+                <div
+                    //className={`${isMapPage ? "w-full h-auto mt-0 pt-1 rounded-sm " : "w-full h-auto mt-0 py-1"} flex flex-col items-center justify-center bg-white border border-3 border-[#388778] px-1.5 shadow-sm mb-0 transition-all duration-200 ml-0`}
+                    className={`${isMapPage ? "w-full h-auto mt-0 pt-1 rounded-sm" : "w-full h-auto mt-0 py-1"
+                        } flex flex-col items-center justify-center bg-white border border-3 border-[#388778] px-1.5 shadow-sm mb-0 transition-all duration-200 ml-0`}
+                >
+                    {userName && (
+                        <div className="text-[9px] text-zinc-400 leading-none mb-0.5 pl-0.5 w-full text-left">
+                            Logged in as: <span className="text-[12px] text-orange-600 font-bold">{userName}</span>
+                        </div>
+                    )}
+                    <div className="w-full flex items-center justify-between gap-1 mb-1 py-0">
+                        <img src="/sdesign_00053.png" alt="買い物かご" className="w-6 aspect-square rounded-full object-contain" />
+                        <span className="flex-1 text-[18px] italic text-[#388778] font-black tracking-wider text-center whitespace-nowrap">Wandering Log</span>
+                        <div className="text-[12px] font-bold text-gray-500 text-right whitespace-nowrap">
+                            {currentPage === "login" && <button onClick={() => window.location.href = "/register"}>新規登録</button>}
+                            {currentPage === "register" && <button onClick={() => window.location.href = "/login"}>ログイン</button>}
+                            {pathname === "/" && <button onClick={handleLogout} className="hover:text-red-500 font-black transition-colors">ログアウト</button>}
+                        </div>
                     </div>
                 </div>
-            </div>
             )}
 
             {/*<div className=" w-full bg-transparent">*/}
